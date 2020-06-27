ffe93e31ee82edbd07d2c965a893fd9e7ac84101
ketansr <srivastavk@aljazeera.net>
2020-06-27 05:35:48 +0530

    Updates

---
+++ precache-manifest.068aed86d88e2835a897333090f5a242.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fab04f07c70ea36b395cd5d724da97",
+    "url": "/churncalculator/index.html"
+  },
+  {
+    "revision": "c51007c090d072c8b6ad",
+    "url": "/churncalculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dab0f25531a0b766dd2f",
+    "url": "/churncalculator/static/js/2.be742763.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/churncalculator/static/js/2.be742763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51007c090d072c8b6ad",
+    "url": "/churncalculator/static/js/main.3fe23465.chunk.js"
+  },
+  {
+    "revision": "0c1feedc633fe01e979d",
+    "url": "/churncalculator/static/js/runtime-main.85853096.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/churncalculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
